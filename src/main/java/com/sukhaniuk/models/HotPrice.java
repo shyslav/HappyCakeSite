6b800614d026e58ff0baddbf75d16e3592054bb2
shyslav <shyslav@gmail.com>
2016-08-19 13:17:26 +0300

    changed git sensetive to case

---
+++ src/main/java/com/sukhaniuk/models/HotPrice.java
@@ -0,0 +1,72 @@
+package com.sukhaniuk.models;
+
+import java.util.Date;
+
+/**
+ * Created by shyshkin_vlad on 22.04.16.
+ */
+public class HotPrice {
+    private int id;
+    private int dishId;
+    private int percent;
+    private String description;
+    private Date dateStart;
+    private Date dateEnd;
+
+    public HotPrice(int id, int dishId, int percent, String description, Date dateStart, Date dateEnd) {
+        this.id = id;
+        this.dishId = dishId;
+        this.percent = percent;
+        this.description = description;
+        this.dateStart = dateStart;
+        this.dateEnd = dateEnd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDishId() {
+        return dishId;
+    }
+
+    public void setDishId(int dishId) {
+        this.dishId = dishId;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+
+    public void setPercent(int percent) {
+        this.percent = percent;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(Date dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public Date getDateEnd() {
+        return dateEnd;
+    }
+
+    public void setDateEnd(Date dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+}
---
+++ src/main/java/com/sukhaniuk/models/Reports.java
@@ -0,0 +1,82 @@
+package com.sukhaniuk.models;
+
+import java.util.Date;
+
+/**
+ * Created by Shyshkin Vladyslav on 05.05.2016.
+ */
+public class Reports {
+    private int id;
+    private String author;
+    private String rText;
+    private Date rDate;
+    private String mail;
+    private String phone;
+    private String vision;
+
+    public Reports(int id, String author, String rText, Date rDate, String mail, String phone, String vision) {
+        this.id = id;
+        this.author = author;
+        this.rText = rText;
+        this.rDate = rDate;
+        this.mail = mail;
+        this.phone = phone;
+        this.vision = vision;
+    }
+
+    public String getVision() {
+        return vision;
+    }
+
+    public void setVision(String vision) {
+        this.vision = vision;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getrText() {
+        return rText;
+    }
+
+    public void setrText(String rText) {
+        this.rText = rText;
+    }
+
+    public Date getrDate() {
+        return rDate;
+    }
+
+    public void setrDate(Date rDate) {
+        this.rDate = rDate;
+    }
+}
