39549e36ab93cc35f032cff451e23e41babfa507
shyslav <shyshkin.vladyslav@gmail.com>
2016-08-20 19:45:44 +0300

    added TestController to output data

--- src/main/java/com/shyslav/controller/HomeController.java
+++ src/main/java/com/shyslav/controller/HomeController.java
@@ -2,17 +2,13 @@ package com.shyslav.controller;
 
 import com.happycake.GlobalController;
 import com.shyslav.data.UserBean;
-import com.shyslav.util.DatabaseConnection;
-import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.SQLException;
 /**
  * Created by shyshkin_vlad on 10.04.16.
@@ -35,30 +31,5 @@ public class HomeController extends GlobalController {
         map.addAttribute("webMenu",headerLoader(request));
         map.addAttribute("contacts",user.getSiteData().getCafeCoordinates());
         return "contacts.jsp";
-    }
-    @RequestMapping(value="lalal")
-    public void lalal(ModelMap map, HttpServletRequest request, HttpServletResponse response) throws IOException, JSONException {
-        UserBean user = getUserInfo(request);
-        map.addAttribute("webTitle","lol");
-
-        String command = "select * from blobTest";
-        DatabaseConnection db = new DatabaseConnection();
-        db.openConnection();
-        InputStream inputStream = null;
-        try {
-            db.rs = db.st.executeQuery(command);
-            while (db.rs.next()) {
-                inputStream = db.rs.getBinaryStream("image");
-            }
-        } catch (Exception ex) {
-            System.out.println(ex);
-        } finally {
-            db.closeConnection();
-        }
-        if(inputStream == null)
-        {
-        }
-        response.getOutputStream().write(IOUtils.toByteArray(inputStream));
-        response.setContentType("image/gif");
     }
 }
